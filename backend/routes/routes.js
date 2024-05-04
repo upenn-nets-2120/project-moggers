37edad0e64e04cf96a78fc735c6b39ac3799d60c
Alicia <asun0102@seas.upenn.edu>
2024-05-04 22:57:10 +0000

    z

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -556,97 +556,7 @@ router.get('/logout', (req, res) => {
 });
 
 
-// *********************************************************
-// only call this method AFTER person 2 ACCEPTS the invite and also we sent the invite.
-router.post('/postChats', async (req, res) => {
-    try {
-        var { chatName, user1, user2 } = req.body;
-        if (!chatName || !user1 || !user2) {
-            return res.status(400).json({error: 'One or more of the fields you entered was empty, please try again.'});
-        }
 
-        // Now check if both users are online
-        var count1 = await db1.send_sql(`SELECT COUNT(*) FROM users WHERE id = "${user1}"`)
-        var count1res = count1[0]['COUNT(*)'];
-    
-        if (count1res != 1) {
-            return res.status(500).json({message: 'Could not find user1 ID in users or found more than one.'});
-        }
-        var status1 = await db1.send_sql(`SELECT status FROM users WHERE id = "${user1}"`);
-        var count2 = await db1.send_sql(`SELECT COUNT(*) FROM users WHERE id = "${user2}"`)
-        var count2res = count2[0]['COUNT(*)'];
-        if (count2res != 1) {
-           
-            return res.status(500).json({message: 'Could not find user2 ID in users or found more than one.'});
-        }
-
-        var status2 = await db1.send_sql(`SELECT status FROM users WHERE id = "${user2}"`);
-        if (!status1 || !status2) {
-            return res.status(500).json({message: 'One or both users are not online, cannot make chat.'});
-        }
-
-
-        var already_exists = false;
-        //////////////////////////////////////////////////////////////////////////////////
-        // Now check if a chat with these two already exists
-        // one that is flipped (user, id )
-        // duplicate it, two filers, one for the first user one for the second user 
-        // merge them back, flip them back join on id 
-        // iterate through id, if any of them have lenght 2, return yes
-        // TODO: update already_exists
-        var x1 = await db1.send_sql(`SELECT * FROM user_chats WHERE user_id = "${user1}"`);
-        var x2 = await db1.send_sql(`SELECT * FROM user_chats WHERE user_id = "${user2}"`);
-       
-        const x1parsed = x1.map(row => ({
-            chat_id: row.chat_id, 
-            user_id: row.user_id
-        }));
-        const x2parsed = x2.map(row => ({
-            chat_id: row.chat_id, 
-            user_id: row.user_id
-        }));
-        const combined = x1parsed.concat(x2parsed);
-        const chatIdsSet = new Set();
-        let hasDuplicates = false;
-
-        for (const item of combined) {
-            if (chatIdsSet.has(item.chat_id)) {
-                hasDuplicates = true;
-                break;
-        } else {
-            chatIdsSet.add(item.chat_id);
-            }
-        }
-
-        
-        if (hasDuplicates) {
-            return res.status(500).json({message: 'A chat already exists with only these two users.'});
-        }
-
-        
-
-        // Otherwise let us insert into the DBs
-        await db1.insert_items(`INSERT INTO chats (name) VALUES ("${chatName}")`);
-        const dbSize = await db1.send_sql('SELECT COUNT(*) FROM user_chats');
-        var new_chat_id = 0;
-        if (dbSize == 1) {
-            // first insert
-            new_chat_id = 1;
-        } else {
-            const max_chat_id = await db1.send_sql(`SELECT MAX(chat_id) FROM user_chats`);
-     
-            new_chat_id = max_chat_id[0]['MAX(chat_id)'] + 1;
-        }
- 
-        await db1.insert_items(`INSERT INTO user_chats (user_id, chat_id) VALUES (${user1}, ${new_chat_id})`);
-        await db1.insert_items(`INSERT INTO user_chats (user_id, chat_id) VALUES (${user2}, ${new_chat_id})`);
-      
-        res.status(200).json({message: "Chat made."});
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message: 'Internal server error'});
-    };
-});
 
 
 
@@ -787,35 +697,125 @@ router.get('/numLikes', async (req, res) => {
 //     res.send(JSON.stringify(kafka_messages));
 // });
 
-// router.post('/joinChat', async (req, res) => {
-//     try {
-//         const room = req.body.room;
+router.post('/joinChat', async (req, res) => {
+    try {
+        const room = req.body.room;
 
-//         // write to database
-//         return res.send({
-//             success: true
-//         });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'Internal server error' });
-//     };
-// });
+        // write to database
+        return res.send({
+            success: true
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    };
+});
 
-// router.post('/leaveChat', async (req, res) => {
-//     try {
-//         const room = req.body.room;
+router.post('/leaveChat', async (req, res) => {
+    try {
+        const room = req.body.room;
 
-//         // write to database
+        // write to database
         
-//         return res.send({
-//             success: true
-//         });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'Internal server error' });
-//     };
-// });
+        return res.send({
+            success: true
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    };
+});
+// *********************************************************
+// only call this method AFTER person 2 ACCEPTS the invite and also we sent the invite.
+router.post('/postChats', async (req, res) => {
+    try {
+        var { chatName, user1, user2 } = req.body;
+        if (!chatName || !user1 || !user2) {
+            return res.status(400).json({error: 'One or more of the fields you entered was empty, please try again.'});
+        }
 
+        // Now check if both users are online
+        var count1 = await db1.send_sql(`SELECT COUNT(*) FROM users WHERE id = "${user1}"`)
+        var count1res = count1[0]['COUNT(*)'];
+    
+        if (count1res != 1) {
+            return res.status(500).json({message: 'Could not find user1 ID in users or found more than one.'});
+        }
+        var status1 = await db1.send_sql(`SELECT status FROM users WHERE id = "${user1}"`);
+        var count2 = await db1.send_sql(`SELECT COUNT(*) FROM users WHERE id = "${user2}"`)
+        var count2res = count2[0]['COUNT(*)'];
+        if (count2res != 1) {
+           
+            return res.status(500).json({message: 'Could not find user2 ID in users or found more than one.'});
+        }
+
+        var status2 = await db1.send_sql(`SELECT status FROM users WHERE id = "${user2}"`);
+        if (!status1 || !status2) {
+            return res.status(500).json({message: 'One or both users are not online, cannot make chat.'});
+        }
+
+
+        var already_exists = false;
+        //////////////////////////////////////////////////////////////////////////////////
+        // Now check if a chat with these two already exists
+        // one that is flipped (user, id )
+        // duplicate it, two filers, one for the first user one for the second user 
+        // merge them back, flip them back join on id 
+        // iterate through id, if any of them have lenght 2, return yes
+        // TODO: update already_exists
+        var x1 = await db1.send_sql(`SELECT * FROM user_chats WHERE user_id = "${user1}"`);
+        var x2 = await db1.send_sql(`SELECT * FROM user_chats WHERE user_id = "${user2}"`);
+       
+        const x1parsed = x1.map(row => ({
+            chat_id: row.chat_id, 
+            user_id: row.user_id
+        }));
+        const x2parsed = x2.map(row => ({
+            chat_id: row.chat_id, 
+            user_id: row.user_id
+        }));
+        const combined = x1parsed.concat(x2parsed);
+        const chatIdsSet = new Set();
+        let hasDuplicates = false;
+
+        for (const item of combined) {
+            if (chatIdsSet.has(item.chat_id)) {
+                hasDuplicates = true;
+                break;
+        } else {
+            chatIdsSet.add(item.chat_id);
+            }
+        }
+
+        
+        if (hasDuplicates) {
+            return res.status(500).json({message: 'A chat already exists with only these two users.'});
+        }
+
+        
+
+        // Otherwise let us insert into the DBs
+        await db1.insert_items(`INSERT INTO chats (name) VALUES ("${chatName}")`);
+        const dbSize = await db1.send_sql('SELECT COUNT(*) FROM user_chats');
+        var new_chat_id = 0;
+        if (dbSize == 1) {
+            // first insert
+            new_chat_id = 1;
+        } else {
+            const max_chat_id = await db1.send_sql(`SELECT MAX(chat_id) FROM user_chats`);
+     
+            new_chat_id = max_chat_id[0]['MAX(chat_id)'] + 1;
+        }
+ 
+        await db1.insert_items(`INSERT INTO user_chats (user_id, chat_id) VALUES (${user1}, ${new_chat_id})`);
+        await db1.insert_items(`INSERT INTO user_chats (user_id, chat_id) VALUES (${user2}, ${new_chat_id})`);
+      
+        res.status(200).json({message: "Chat made."});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Internal server error'});
+    };
+});
 
 router.get('/getConvos', async (req, res) => {
     try {
@@ -852,6 +852,7 @@ router.get('/getConvos', async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     };
 });
+
 
 router.get('/getMessages', async (req, res) => {
     try {

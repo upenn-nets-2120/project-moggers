50e267d656336fb1d4cd3afbf9fea4de2ff363ad
Matthew <matsu@sas.upenn.edu>
2024-05-05 02:24:49 +0000

    env

--- backend/routes/register_routes.js
+++ backend/routes/register_routes.js
@@ -8,6 +8,8 @@ function register_routes(app) {
     app.get('/register', routes.get_register);
     app.post('/login', routes.post_login);
     app.post('/logout', routes.post_logout); 
+    // app.get('/getMessages', routes.get_register);
+    // app.post('/postMessage', routes.post_logout); 
    
     // TODO: register getMovie, which does not need a :username
     //       Make it compatible with the call from ChatInterface.tsx

92b372f0ca7371a10a78ab007acc5bb703260d21
Haokun He <haokunhe@seas.upenn.edu>
2024-05-08 06:34:27 +0000

    added thread.sleep

--- backend/socialrank/src/main/java/edu/upenn/cis/nets2120/hw3/local/ComputeRanksLocal.java
+++ backend/socialrank/src/main/java/edu/upenn/cis/nets2120/hw3/local/ComputeRanksLocal.java
@@ -26,37 +26,46 @@ public class ComputeRanksLocal {
         double d_max;
         int i_max;
 
-        // Process command line arguments if given
-        if (args.length == 1) {
-            d_max = Double.parseDouble(args[0]);
-            i_max = 25;
-            debug = false;
-        } else if (args.length == 2) {
-            d_max = Double.parseDouble(args[0]);
-            i_max = Integer.parseInt(args[1]);
-            debug = false;
-        } else if (args.length == 3) {
-            d_max = Double.parseDouble(args[0]);
-            i_max = Integer.parseInt(args[1]);
-            debug = true;
-        } else {
-            d_max = 30;
-            i_max = 25;
-            debug = false;
-        }
-
-        ComputeRanks job = new ComputeRanks(d_max, i_max, 1000, debug);
-
-        List<Tuple2<String, Tuple2<String, Double>>> topK = job.mainLogic();
-        logger.info("*** Finished social network ranking! ***");
-
-        try (PrintStream out = new PrintStream(new FileOutputStream("socialrank-local.csv"))) {
-            for (Tuple2<String, Tuple2<String, Double>> item : topK) {
-                out.println(item._1 + "," + item._2._1 + "," + item._2._2);
-                logger.info(item._1 + " " + item._2._1 + " " + item._2._2);
+        while (true) {
+            // Process command line arguments if given
+            if (args.length == 1) {
+                d_max = Double.parseDouble(args[0]);
+                i_max = 25;
+                debug = false;
+            } else if (args.length == 2) {
+                d_max = Double.parseDouble(args[0]);
+                i_max = Integer.parseInt(args[1]);
+                debug = false;
+            } else if (args.length == 3) {
+                d_max = Double.parseDouble(args[0]);
+                i_max = Integer.parseInt(args[1]);
+                debug = true;
+            } else {
+                d_max = 30;
+                i_max = 25;
+                debug = false;
             }
-        } catch (Exception e) {
-            logger.error("Error writing to file: " + e.getMessage());
+
+            ComputeRanks job = new ComputeRanks(d_max, i_max, 1000, debug);
+
+            List<Tuple2<String, Tuple2<String, Double>>> topK = job.mainLogic();
+            logger.info("*** Finished social network ranking! ***");
+
+            try (PrintStream out = new PrintStream(new FileOutputStream("socialrank-local.csv"))) {
+                for (Tuple2<String, Tuple2<String, Double>> item : topK) {
+                    out.println(item._1 + "," + item._2._1 + "," + item._2._2);
+                    logger.info(item._1 + " " + item._2._1 + " " + item._2._2);
+                }
+            } catch (Exception e) {
+                logger.error("Error writing to file: " + e.getMessage());
+            }
+
+            try {
+                System.out.println("Sleeping for an hour.");
+                Thread.sleep(1000 * 60 * 60);
+            } catch (InterruptedException e) {
+                logger.error("Interrupted: " + e.getMessage());
+            }
         }
     }
 

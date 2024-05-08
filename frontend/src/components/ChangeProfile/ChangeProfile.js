3f7c968cf791f96d53c7fc63a8bcbb4949a464ab
Alicia <asun0102@seas.upenn.edu>
2024-05-08 21:10:31 +0000

    change profile page

---
+++ frontend/src/components/ChangeProfile/ChangeProfile.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import ReactSession from '../../ReactSession';
+import Profile from '../Profile/Profile';
+import config from '../../serverConfig.json';
+
+const ChangeProfile = () => {
+
+
+    return (
+        <div className="ChangeProfilePage">
+            
+        </div>
+    );
+}
+
+export default ChangeProfile;

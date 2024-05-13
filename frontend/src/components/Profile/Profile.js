3e72c8477e3f690c12df0a07afa91e44a2cce0e0
ashleyzhang01 <ashleyzhang10@gmail.com>
2024-05-13 01:05:15 -0400

    search add friends

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
@@ -8,22 +8,27 @@ import styles from './Profile.module.css';
 import ReactSession from '../../ReactSession';
 
 function Profile() {
-  var { user_id } = useParams();
-  if (!user_id || user_id === -1 || user_id === "undefined") { 
-    user_id = ReactSession.get("user_id");
+  const { user_id: paramUserId } = useParams();
+  let currUserId;
+  if (!paramUserId || paramUserId === -1 || paramUserId === "undefined") { 
+    currUserId = ReactSession.get("user_id");
   }
   const [profileData, setProfileData] = useState(null);
   const [profilePhoto, setProfilePhoto] = useState("https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png");
+  const [searchUsername, setSearchUsername] = useState('');
+  const [searchedUserId, setSearchedUserId] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProfile = async () => {
       try {
+        const userId = paramUserId || currUserId;
         if (ReactSession.get("user_id") === -1) {
           console.error('Please log in to view profile');
           return;
         }
         const response = await axios.get(`${config.serverRootURL}/getProfile`, {
-          params: { user_id: user_id }
+          params: { user_id: userId }
           // params: {user_id: 2}
         });
         setProfileData(response.data);
@@ -35,11 +40,40 @@ function Profile() {
       }
     };
     fetchProfile();
-  }, [user_id]);
+  }, [paramUserId, currUserId]);
 
   if (!profileData) {
     return <div style={{textAlign: "center"}}>Loading...</div>;
   }
+
+  const handleSearchSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(`${config.serverRootURL}/getUserName`, {
+        params: { username: searchUsername }
+      });
+      const { id } = response.data.data;
+      if (id !== -1) {
+        navigate(`/profile/${id}`);
+      } else {
+        console.error('User not found');
+      }
+    } catch (error) {
+      console.error('Error searching for user:', error);
+    }
+  };
+
+  const handleAddFriend = async () => {
+    try {
+      await axios.post(`${config.serverRootURL}/sendFriendRequest`, {
+        follower: ReactSession.get("user_id"),
+        followed: paramUserId
+      });
+      console.log('Friend request sent successfully');
+    } catch (error) {
+      console.error('Error sending friend request:', error);
+    }
+  };
 
   const formatDate = (date) => {
     const options = { month: 'short', day: 'numeric' };
@@ -49,7 +83,18 @@ function Profile() {
   return (
     <div className={styles.container}>
       <div className={styles.profileInfo}>
-        <h1>Welcome, {profileData.data1[0].username}</h1>
+      <form onSubmit={handleSearchSubmit}>
+            <input
+              style={{width: "80%", borderRadius: "5px", marginTop: "10px", marginBottom: "20px", padding: "5px", border: "1px solid lightgray"}}
+              type="text"
+              value={searchUsername}
+              onChange={(e) => setSearchUsername(e.target.value)}
+              placeholder="Search by username"
+            />
+            <button style={{border: "None", borderRadius: "5px", marginLeft: "10px", background: "blue", color: "white"}} type="submit">Search</button>
+        </form>
+      {(paramUserId === currUserId || paramUserId === undefined) && (
+        <h1>Welcome, {profileData.data1[0].username}</h1> )}
         <div className={styles.profileHeader}>
           <div className={styles.profileLeft}>
             <img src={profileData.data1[0].profilePhoto || profilePhoto} alt="Profile" className={styles.profilePic} />
@@ -69,6 +114,11 @@ function Profile() {
             </div>
             <div className={styles.profileRightBottom}>
               <p><b>Interests</b>: {profileData.data1[0].interests}</p>
+            </div>
+            <div className={styles.profileRightBottom}>
+              {paramUserId && currUserId === profileData.data1[0].id && (
+                <button style={{borderRadius: '5px', backgroundColor: 'blue', color: 'white', marginRight: '20px', marginBottom: '10px'}} onClick={handleAddFriend}>Add Friend</button>
+              )}
             </div>
           </div>
         </div>
